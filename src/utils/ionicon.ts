0ee027d733695268c620e3513bd44a7f7ebac44b
Guilherme Tomiasi <gc.tomiasi@unesp.br>
2022-04-10 00:15:17 -0300

    gambiarra para usar Ionicon recebe a string svg a partir da string que dá o nome do ícone

---
+++ src/utils/ionicon.ts
@@ -0,0 +1,8 @@
+import * as Ionicons from "ionicons/icons";
+
+function getIoniconByString (icon: string) {
+  const IoniconsByString = Ionicons as Record<string, string | undefined>;
+  return IoniconsByString[icon];
+}
+
+export default getIoniconByString;
